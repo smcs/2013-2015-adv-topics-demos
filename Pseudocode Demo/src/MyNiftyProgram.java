ac2b369e1749ad17601cf3745f380c056fc2fe8f
Seth Battis <SethBattis@stmarksschool.org>
2013-11-08 07:42:31 -0500

    Pseudocode Demo
    Example of how to "pre-document" a program.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ Pseudocode Demo/src/MyNiftyProgram.java
@@ -0,0 +1,46 @@
+/**
+ * A brief (but complete) high-level description of the function and purpose
+ * of the class, included as Javadoc documentation for each class.
+ * 
+ * @author Seth Battis
+ *
+ */
+public class MyNiftyProgram {
+	
+	/**
+	 * A description of each instance variable/data field that you expect
+	 * to employ in the class, along with a tentative variable
+	 * declaration suggesting type. (Recall that good naming conventions
+	 * will allow you to skimp on additional documentation.)
+	 */
+	private FancyPantsObject splendidTrousers;
+	
+	/**
+	 * A brief (but complete) high-level description of the major methods
+	 * that you expect this class to require. These methods should,
+	 * essentially, be the "verbs" that describe what this object can do.
+	 * 
+	 * @param severityOfCrease ranges from 0-255
+	 */
+	public void creaseThoseTrousers(double severityOfCrease) {
+		
+	}
+	
+	/**
+	 * Some methods may require more visual documentation. If this is the
+	 * case, please, by all means, include that visual documentation as
+	 * an image (or video!) file in your project repository. You can
+	 * employ standard Javadoc semantics to reference images from your
+	 * documentation (can = must, BTW). As in:
+	 * 
+	 * @see https://stmarksschool.instructure.com/courses/1238
+	 * 
+	 * or
+	 * 
+	 * @see nameOfMyImageFile.png
+	 */
+	public void intricateFoldingStyle() {
+		
+	}
+
+}

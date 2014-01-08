6ffd7aea9bddee1bdd3c0942de57b697570172c3
Seth Battis <SethBattis@stmarksschool.org>
2014-01-08 08:44:33 -0500

    SimplerJOGL Demo
    A quick demonstration of the basic setup and syntax of a SimplerJOGL app.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

---
+++ SimplerJOGL Demo/src/joglapp/JOGLApp.java
@@ -0,0 +1,15 @@
+
+package joglapp;
+
+import simplerjogl.*;
+
+public class JOGLApp
+{
+	public static void main (String[] args)
+	{
+		Renderer renderer = new JOGLRenderer ();
+		Frame frame = Frame.createFrame ("SimplerJOGL App", false);
+		frame.addGLEventListener (renderer);
+		frame.start ();
+	}
+}

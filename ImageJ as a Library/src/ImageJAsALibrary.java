0ec7acb05721a16b21a38b4a807fe3290a1108bb
Seth Battis <SethBattis@stmarksschool.org>
2014-03-07 09:17:03 -0500

    Documentation
    Now with explanation!
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- ImageJ as a Library/src/ImageJAsALibrary.java
+++ ImageJ as a Library/src/ImageJAsALibrary.java
@@ -4,11 +4,23 @@ import ij.io.*;
 public class ImageJAsALibrary {
 
 	public static void main(String[] args) {
+		/*
+		 * An opener is just a utility object to open images -- it could easily
+		 * be connected to an open dialog instead of a hard-coded file path
+		 */
 		Opener file = new Opener();
+
+		/* An ImagePlus is an object that represents the loaded image */
 		ImagePlus a = file.openImage("images/A.png");
-		ImagePlus b = file.openImage("images/B.png");
+
+		/*
+		 * the pixel data represents the pixel as RGBA (Red, Green, Blue and
+		 * Alpha) channels, where each channel has a value 0-255, and alpha
+		 * represents transparency (0 is opaque, 255 is completely transparent)
+		 */
 		int[] pixelData = a.getPixel(36, 25);
-		System.out.println(pixelData[0] + " " + pixelData[1] + " " + pixelData[2] + " " + pixelData[3]);
+		System.out.println(pixelData[0] + " " + pixelData[1] + " "
+				+ pixelData[2] + " " + pixelData[3]);
 	}
 
 }

64945e5821747c63f0b3df0e61e46272d0f956cd
Seth Battis <SethBattis@stmarksschool.org>
2014-04-04 07:35:49 -0400

    Run a Command
    Simplified the opening of an image to just use the ImageJ app. This may introduce some limitations — such as needing to have the image window open to run processing commands on it. But it streamlines the code, and we probably want windows open so that we can debug, for now.
    
    Also demonstrated calling a command on the opened image in ImageJ, converting it to 8-bit color.
    
    Albert Cardona has a [a good set of tutorials](http://albert.rierol.net/imagej_programming_tutorials.html) that got me here. Not directly, but they gave me enough hints to guess this.
    
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- ImageJ as a Library/src/ImageJAsALibrary.java
+++ ImageJ as a Library/src/ImageJAsALibrary.java
@@ -1,26 +1,27 @@
 import ij.*;
 import ij.io.*;
+import ij.process.ImageProcessor;
 
 public class ImageJAsALibrary {
 
 	public static void main(String[] args) {
-		/*
-		 * An opener is just a utility object to open images -- it could easily
-		 * be connected to an open dialog instead of a hard-coded file path
-		 */
-		Opener opener = new Opener();
+		/* An ImagePlus is an object that represents the loaded image. IJ is the ImageJ application. */
+		ImagePlus image = IJ.openImage("images/A.png");
 
-		/* An ImagePlus is an object that represents the loaded image */
-		ImagePlus a = opener.openImage("images/A.png");
-
+		/* display the image we're working with in its own window -- this also puts ImageJ's focus on this image, so that subsequent commands will run on this image. */
+		image.show();
+		
 		/*
 		 * the pixel data represents the pixel as RGBA (Red, Green, Blue and
 		 * Alpha) channels, where each channel has a value 0-255, and alpha
 		 * represents transparency (0 is opaque, 255 is completely transparent)
 		 */
-		int[] pixelData = a.getPixel(36, 25);
+		int[] pixelData = image.getPixel(36, 25);
 		System.out.println(pixelData[0] + " " + pixelData[1] + " "
 				+ pixelData[2] + " " + pixelData[3]);
+		
+		/* convert the current image to 8-bit -- calling the command by its exact name in the menu */
+		IJ.run("8-bit");
 	}
 
 }

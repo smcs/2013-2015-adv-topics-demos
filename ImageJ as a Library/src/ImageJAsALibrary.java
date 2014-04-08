459195e5ca4f8d63b3e9a5d33640e9524a024c9e
Seth Battis <SethBattis@stmarksschool.org>
2014-04-08 12:44:20 -0400

    Updated documentation
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- ImageJ as a Library/src/ImageJAsALibrary.java
+++ ImageJ as a Library/src/ImageJAsALibrary.java
@@ -5,12 +5,19 @@ import ij.process.ImageProcessor;
 public class ImageJAsALibrary {
 
 	public static void main(String[] args) {
-		/* An ImagePlus is an object that represents the loaded image. IJ is the ImageJ application. */
+		/*
+		 * An ImagePlus is an object that represents the loaded image. IJ is the
+		 * ImageJ application.
+		 */
 		ImagePlus image = IJ.openImage("images/A.png");
 
-		/* display the image we're working with in its own window -- this also puts ImageJ's focus on this image, so that subsequent commands will run on this image. */
+		/*
+		 * display the image we're working with in its own window -- this also
+		 * puts ImageJ's focus on this image, so that subsequent commands will
+		 * run on this image.
+		 */
 		image.show();
-		
+
 		/*
 		 * the pixel data represents the pixel as RGBA (Red, Green, Blue and
 		 * Alpha) channels, where each channel has a value 0-255, and alpha
@@ -19,8 +26,11 @@ public class ImageJAsALibrary {
 		int[] pixelData = image.getPixel(36, 25);
 		System.out.println(pixelData[0] + " " + pixelData[1] + " "
 				+ pixelData[2] + " " + pixelData[3]);
-		
-		/* convert the current image to 8-bit -- calling the command by its exact name in the menu */
+
+		/*
+		 * convert the current image to 8-bit -- calling the command by its
+		 * exact name in the menu
+		 */
 		IJ.run("8-bit");
 	}
 

e75a9fbc144071214b9e0c7615482061bb87308e
Seth Battis <SethBattis@stmarksschool.org>
2014-10-06 17:24:43 -0400

    Web VisibleImage
    Signed-off-by: Seth Battis <SethBattis@stmarksschool.org>

--- ObjectDraw VisibleImages/src/ImageDisplay.java
+++ ObjectDraw VisibleImages/src/ImageDisplay.java
@@ -10,8 +10,16 @@ public class ImageDisplay extends WindowController {
 		Image png = getImage("portable-network-graphic.png");
 		Image gif = getImage("graphics-interchange-format.gif");
 		Image jpg = getImage("joint-photographic-experts-group.jpg");
+
+		/*
+		 * it seems that you can load images from the web, so long as the URL is
+		 * not "complicated" -- that is, so long as it doesn't involve say,
+		 * #anchors or ?form=requests
+		 */
+		Image web = getImage("https://en.gravatar.com/userimage/696768/f1260feccb38f9a5a3f5aad91ba6d49f.jpeg");
 		new VisibleImage(png, 10, 10, canvas);
 		new VisibleImage(gif, 60, 10, canvas);
 		new VisibleImage(jpg, 110, 10, canvas);
+		new VisibleImage(web, 10, 70, canvas);
 	}
 }

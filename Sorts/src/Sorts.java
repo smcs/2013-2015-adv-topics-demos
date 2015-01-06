8a2421771ff12d9385c8dd945aa457ee6438955d
Seth Battis <sethbattis@stmarksschool.org>
2015-01-06 14:41:16 -0500

    Instrumented Sorts

--- Sorts/src/Sorts.java
+++ Sorts/src/Sorts.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-public class BubbleSort {
-
+public class Sorts {
+	
 	/**
 	 * Perform a a bubble sort on an array of presumptively unsorted integers
 	 * 
@@ -12,37 +12,50 @@ public class BubbleSort {
 	 *      values
 	 * @post unsorted is unchanged
 	 */
-	public static Vector<Integer> bubbleSort(Vector<Integer> unsorted) {
+	public static Vector<Integer> bubbleSort(Vector<Integer> unsorted, Statistic stat) {
 		Vector<Integer> sorted = new Vector<Integer>(unsorted);
+		stat.updateStorage(unsorted.size());
 		boolean didISwap = false;
 		int bubbleThumb;
 
 		/* keep repeating until we have no more swaps to make */
+		stat.startTimer();
 		do {
 			didISwap = false;
 			/*
-			 * one pass throug the array, floating the current highest value to
+			 * one pass through the array, floating the current highest value to
 			 * the end
 			 */
 			for (bubbleThumb = 0; bubbleThumb < sorted.size() - 1; bubbleThumb++) {
 				assert (sorted.get(bubbleThumb) != null);
 				assert (sorted.get(bubbleThumb + 1) != null);
+				stat.addRead();
+				stat.addRead();
 				if (sorted.get(bubbleThumb) > sorted.get(bubbleThumb + 1)) {
 					didISwap = true;
 					int temp = sorted.get(bubbleThumb);
+					stat.updateStorage(sorted.size() + 1);
 					sorted.set(bubbleThumb, sorted.get(bubbleThumb + 1));
+					stat.addRead();
+					stat.addRead();
+					stat.addWrite();
 				sorted.set(bubbleThumb + 1, temp);
 				} else {
 					// thumb moves over 1, without a swap
 				}
 			}
 		} while (didISwap);
+		stat.stopTimer();
 		return sorted;
 	}
 
 	public static void main(String[] args) {
-		Vector<Integer> mess = Sorting.randomVector(20);
-		Sorting.printArray(mess);
-		Sorting.printArray(bubbleSort(mess));
+		for (int i = 0; i < 100; i++) {
+			Vector<Integer> mess = Sorting.randomVector(1000);
+			Sorting.printArray(mess);
+			Statistic stat = new Statistic("Bubble Sort", mess.size(), 0, 0);
+			Sorting.printArray(bubbleSort(mess, stat));
+			System.out.println(stat);
+		}
 	}
 }
---
+++ Sorts/src/Statistic.java
@@ -0,0 +1,52 @@
+
+public class Statistic {
+	private int numberOfBrokenKeyboards, quantityOfDeveloperTears;
+	private int n; // number of elements
+	private double runTime, start, stop; // in milliseconds
+	private long storage; // in bytes
+	private int reads, writes;
+	private String algorithm;
+	
+	public Statistic(String algorithm, int n, int keyboards, int tears) {
+		this.algorithm = algorithm;
+		this.numberOfBrokenKeyboards = keyboards;
+		this.quantityOfDeveloperTears = tears;
+		this.n = n;
+		storage = 0;
+		reads = 0;
+		writes = 0;
+		runTime = 0;
+	}
+	
+	public void updateStorage(long newStorage) {
+		if (newStorage > storage) {
+			storage = newStorage;
+		}
+	}
+	
+	public void startTimer() {
+		start = System.currentTimeMillis();
+	}
+	
+	public void stopTimer() {
+		stop = System.currentTimeMillis();
+		runTime = stop - start;
+	}
+	
+	public void addRead() {
+		reads++;
+	}
+	
+	public void addWrite() {
+		writes++;
+	}
+	
+	public String toString() {
+		return new String(
+				algorithm + ": " + n + " elements\n" +
+				"runtime: " + runTime + " milliseconds\n" +
+				"storage: " + storage + " bytes max, " + reads + " reads, " + writes + " writes\n" +
+				"anguish: " + numberOfBrokenKeyboards + " keyboards broken / " + quantityOfDeveloperTears + " tears shed\n"
+			);
+	}
+}
